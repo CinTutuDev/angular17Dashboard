f716a2f34c34f99dddf62cbc5b6d1a6f5a6c4deb
cinta <cintatafur@gmail.com>
2024-01-30 14:06:08 +0100

    🧱 Create(app structure)

---
+++ src/app/shared/heavy-loaders/heavy-loaders-low.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-low',
+  standalone: true,
+  imports: [CommonModule],
+  template:`<h1>Hola Cinta</h1>`,
+
+})
+export class HeavyLoadersLowComponent {
+
+}
