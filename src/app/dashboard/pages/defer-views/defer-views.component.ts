4ae2903646c49cb7b6606648abfd974b091add49
cinta <cintatafur@gmail.com>
2024-01-31 15:04:33 +0100

    ➕ Defer(defer blocks)

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersLowComponent } from '@shared/heavy-loaders/heavy-loaders-low.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent, HeavyLoadersLowComponent],
   templateUrl: './defer-views.component.html',
   styles: ``
 })
--- src/app/shared/heavy-loaders/heavy-loaders-low.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-low.component.ts
@@ -1,13 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-low',
   standalone: true,
   imports: [CommonModule],
-  template:`<h1>Hola Cinta</h1>`,
+  template:`
+    <section [ngClass]="['w-full h-[600px]  text-center', cssClass]">
+      Heavy Loaders Low
+    </section>
+
+  `,
 
 })
 export class HeavyLoadersLowComponent {
+
+  @Input({required: true}) cssClass!: string
+
+  constructor(){
+    const start = Date.now();
+    while (Date.now() - start < 3000) {    }
+    console.log('Cargado');
+  }
 
 }
