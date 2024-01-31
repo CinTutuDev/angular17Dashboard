e0fa616fec7005b5b9721fd6b6100b3de88f6e3b
cinta <cintatafur@gmail.com>
2024-01-31 15:47:21 +0100

    👥 @defer(defer triggers - interactions)

--- src/app/shared/heavy-loaders/heavy-loaders-fast.components.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.components.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
   imports: [CommonModule],
-  template:`<h1>Hola Cinta fast</h1>`,
-
+  template: `
+    <section [ngClass]="['w-full h-[600px]  text-center', cssClass]">
+      <ng-content />
+    </section>
+  `,
 })
 export class HeavyLoadersFastComponent {
+  @Input({ required: true }) cssClass!: string;
 
+  constructor(){
+    console.log('HeavyLoader creado');
+  }
 }
