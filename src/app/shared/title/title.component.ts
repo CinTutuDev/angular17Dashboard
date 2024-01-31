11874ad2735808f00097c693aae55dc7fe52a933
cinta <cintatafur@gmail.com>
2024-01-31 13:27:39 +0100

    ✅ @Input(Required)

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
-  templateUrl: './title.component.html',
-  styleUrl: './title.component.scss'
+  template: `<h2 class="mb-7 font-bold text-3xl text-gray-600 text-center">
+    {{ title }} <!-- - {{withSahdow}} -->
+  </h2>`,
+  styleUrl: './title.component.scss',
 })
 export class TitleComponent {
-
+  @Input({required: true}) title!: string;
+  @Input({transform: booleanAttribute}) withSahdow: boolean = false;
 }
