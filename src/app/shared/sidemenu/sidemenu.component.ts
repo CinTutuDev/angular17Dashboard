bbc6476e2fa6532bacd537dbfd58ba842784402a
cinta <cintatafur@gmail.com>
2024-01-31 09:41:56 +0100

    ♻️ MenuItem(new syntax @for)

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-sidemenu',
+  selector: 'sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
-  styleUrl: './sidemenu.component.scss'
+  styleUrl: './sidemenu.component.scss',
 })
 export class SidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
 
+  constructor() {
+    /*     const dashboardRoutes = routes
+      .map((route) => route.children ?? [])
+      .flat()
+      .filter((route) => route && route.path)
+      .filter(route => !route.path?.includes(':'))
+
+    console.log(dashboardRoutes); */
+  }
 }
