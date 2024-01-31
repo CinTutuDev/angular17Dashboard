33210cdd959083049d6c3129cf176edfde9c40f8
cinta <cintatafur@gmail.com>
2024-01-31 12:33:36 +0100

    ✅ @for(control flow)

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,22 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
-type Grade = 'A'| 'B'| 'F';
+type Grade = 'A' | 'B' | 'F';
 @Component({
   standalone: true,
   imports: [CommonModule],
   templateUrl: './control-flow.component.html',
-  styles: ``
+  styles: ``,
 })
 export default class ControlFlowComponent {
-
   public showContent = signal(false);
   public grade = signal<Grade>('A');
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Qwik', ' React']);
+  public frameworks2 = signal([]);
 
-  public myName : string = 'Cinta';
+  public myName: string = 'Cinta';
 
-  public toggleContent(){
-    this.showContent.update(value => !value)
+  public toggleContent() {
+    this.showContent.update((value) => !value);
   }
-
 }
