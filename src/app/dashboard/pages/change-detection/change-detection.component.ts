0608443fc34afbf8a97fc2c56864beb048a0930b
cinta <cintatafur@gmail.com>
2024-02-01 09:06:54 +0100

    💄 Changes(Styles)

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -13,8 +13,40 @@ import { TitleComponent } from '@shared/title/title.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <app-title [title]="currentFramework()" />
-    <pre>{{ frameworkAsSignal() | json }}</pre>
-    <pre>{{ frameworkAsProperty | json }}</pre>
+    <!--  <pre>{{ frameworkAsSignal() | json }}</pre>
+    <pre>{{ frameworkAsProperty | json }}</pre> -->
+
+    <div
+      class="tab bg-gray-100 font-sans flex h-screen items-center justify-center"
+    >
+      <div x-data="{ openTab: 1 }" class="p-8">
+        <div class="max-w-md mx-auto">
+          <div
+            x-show="openTab === 1"
+            class="transition-all duration-300 bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-600"
+          >
+            <h2 class="text-2xl font-semibold mb-2 text-blue-600">
+              Propiedad tradicional
+            </h2>
+            <p class="text-gray-700">{{ frameworkAsSignal() | json }}</p>
+          </div>
+
+          <div
+            x-show="openTab === 2"
+            class="transition-all duration-300 bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-600"
+          >
+            <h2 class="text-2xl font-semibold mb-2 text-blue-600">
+             Propiedad con signal()
+            </h2>
+            <pre class="text-gray-700">{{ frameworkAsSignal() | json }}</pre>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js"
+      defer
+    ></script>
   `,
   styles: ``,
 })
